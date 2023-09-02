e3bd94eac5f629f4a0981818012af4cfaa5ee8d6
Ilya Sakhatskiy <48794031+FatLarva@users.noreply.github.com>
2023-09-03 01:31:00 +0400

    Added ability to automatically pass button click through ViewModel in a form of executing command,

--- ViewsSourceGenerator/Templates/ViewClass/ButtonMethodCallInfo.cs
+++ ViewsSourceGenerator/Templates/ViewClass/ButtonMethodCallInfo.cs
@@ -5,12 +5,30 @@ namespace ViewsSourceGenerator
         public readonly string ButtonFieldName;
         public readonly string MethodToCall;
         public readonly bool ShouldGenerateMethodWithPartialStuff;
+        private readonly AutoCreationInfo _autoCreationInfo;
 
-        public ButtonMethodCallInfo(string buttonFieldName, string methodToCall, bool shouldGenerateMethodWithPartialStuff)
+        public bool HasPrivateCreations => _autoCreationInfo.HasPrivateCreations;
+
+        public bool HasPublicCreations => _autoCreationInfo.HasPublicCreations;
+
+        public bool HasObservablesToDispose => HasPrivateCreations;
+        
+        public bool HasPassForwardCommands => !_autoCreationInfo.IsEmpty;
+        
+        public ButtonMethodCallInfo(string buttonFieldName, string methodToCall, bool shouldGenerateMethodWithPartialStuff, AutoCreationInfo autoCreationInfo)
         {
             ButtonFieldName = buttonFieldName;
             MethodToCall = methodToCall;
             ShouldGenerateMethodWithPartialStuff = shouldGenerateMethodWithPartialStuff;
+            _autoCreationInfo = autoCreationInfo;
         }
+        
+        public string GetAutoCreatedObserversPrivatePart() => _autoCreationInfo.GetAutoCreatedObserversPrivatePart();
+
+        public string GetAutoCreatedObserversDisposePart() => _autoCreationInfo.GetAutoCreatedObserversDisposePart();
+
+        public string GetAutoCreatedObserversPublicPart() => _autoCreationInfo.GetAutoCreatedObserversPublicPart();
+        
+        public string GetCallingCommandPart() => _autoCreationInfo.GetCallingCommandPart();
     }
 }
