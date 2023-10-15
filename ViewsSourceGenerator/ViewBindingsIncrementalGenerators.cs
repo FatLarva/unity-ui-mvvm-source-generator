8e8753de5a96561e2cf65e592ee3ff5e1d34e4fc
Ilya Sakhatskiy <48794031+FatLarva@users.noreply.github.com>
2023-10-15 23:53:20 +0400

    Remove additional attribute project since incremental source generators solves the problem of duplicating attributes. Also add nullable context to generated files.

---
+++ ViewsSourceGenerator/ViewBindingsIncrementalGenerators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace ViewsSourceGenerator
+{
+    [Generator]
+    public class ViewBindingsIncrementalGenerators : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(
+                pIContext =>
+                {
+                    Console.Out.WriteLine($"Hello from Incremental source generator");
+                    pIContext.AddSource(ViewModelMethodCallAttributeTemplate.SourceFileName, new ViewModelMethodCallAttributeTemplate().TransformText());
+                    pIContext.AddSource(ViewModelGenerateAttributeTemplate.SourceFileName, new ViewModelGenerateAttributeTemplate().TransformText());
+                    pIContext.AddSource(LocalizeWithKeyAttributeTemplate.SourceFileName, new LocalizeWithKeyAttributeTemplate().TransformText());
+                    pIContext.AddSource(LocalizeWithKeyFromFieldAttributeTemplate.SourceFileName, new LocalizeWithKeyFromFieldAttributeTemplate().TransformText());
+                    pIContext.AddSource(SubscribeOnViewModelsObservableAttributeTemplate.SourceFileName, new SubscribeOnViewModelsObservableAttributeTemplate().TransformText());
+                    pIContext.AddSource(BindToObservableAttributeTemplate.SourceFileName, new BindToObservableAttributeTemplate().TransformText());
+                    pIContext.AddSource(BindingTypeEnumTemplate.SourceFileName, new BindingTypeEnumTemplate().TransformText());
+                    pIContext.AddSource(AutoCreationFlagEnumTemplate.SourceFileName, new AutoCreationFlagEnumTemplate().TransformText());
+                    pIContext.AddSource(GeneratedViewModelAttributeTemplate.SourceFileName, new GeneratedViewModelAttributeTemplate().TransformText());
+                    pIContext.AddSource(SubViewAttributeTemplate.SourceFileName, new SubViewAttributeTemplate().TransformText());
+                    pIContext.AddSource(CommonModelAttributeTemplate.SourceFileName, new CommonModelAttributeTemplate().TransformText());
+                    pIContext.AddSource(GeneratedModelAttributeTemplate.SourceFileName, new GeneratedModelAttributeTemplate().TransformText());
+                });
+        }
+    }
+}
